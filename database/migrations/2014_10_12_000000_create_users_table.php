25ab9669e573af6754396818b30d2448ad14e03a
Mukengeshayi <desiremukengeshayi27@gmail.com>
2023-03-27 06:37:07 +0100

    creations de donnees de base et installation du package nestedSet

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->enum('role',array('user','redac','admin'))->default('user');
+            $table->boolean('valid')->default(false);
+            
         });
     }
 
